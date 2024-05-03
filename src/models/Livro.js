560ff64bc32de8f13fd1ef7df91de02c537bbe51
Matheus-santos7 <matheussantosgc@icloud.com>
2024-05-03 05:28:45 -0300

    Feat: custom validation mongoose fields

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,11 +2,35 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
   {
-    id: { type: String },
-    titulo: { type: String, required: [true, "O titulo do livro é obrigatorio"] },
-    autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O nome do autor(a) é obrigatorio"] },
-    editora: { type: String, required: [true, "O nome da editora é obrigatorio"] },
-    numeroPaginas: { type: Number },
+    id: { 
+      type: String 
+    },
+    titulo: { type: String,
+      required: [true, "O titulo do livro é obrigatorio"] 
+    },
+    autor: { 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores",
+      required: [true, "O nome do autor(a) é obrigatorio"] 
+    },
+    editora: { 
+      type: String, 
+      required: [true, "O nome da editora é obrigatorio"],
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "As editoras {VALUE} não são aceitas"
+
+      }
+    },
+    numeroPaginas: { 
+      type: Number, 
+      validate:{
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    },
   },
 );
 
