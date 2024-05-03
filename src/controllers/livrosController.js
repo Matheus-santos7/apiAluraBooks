4320abe9880114449388d1998923dc8fc66e26d7
Matheus-santos7 <matheussantosgc@icloud.com>
2024-05-03 06:51:19 -0300

    refactor: using regex through mongoose

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -82,15 +82,13 @@ class LivroController {
     try {
       const { editora, titulo } = req.query;
 
-      const regex = new RegExp(titulo, "i");
-
       const busca = {};
 
       if (editora) {
         busca.editora = editora;
       }
       if (titulo) {
-        busca.titulo = regex;
+        busca.titulo = { $regex: titulo, $options: "i" };
       }
 
       const livrosResultado = await livros.find(busca);
